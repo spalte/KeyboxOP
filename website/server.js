5e9c9ccee7d2ae3e5c579e18bf023cca1fffa683
Joël Spaltenstein <spalte@naturalimage.ch>
2021-02-05 18:17:37 +0100

    work

---
+++ website/server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+const nocache = require('nocache');
+
+var static = express.static(path.join(__dirname, 'public'));
+app.use(static);
+app.use(nocache());
+
+app.get("/oidc-client.js", function(req, res){
+    res.sendFile(path.join(__dirname, '../oidc-client.min.js'));
+});
+
+app.get("/vanillajs.png", function(req, res){
+    res.sendFile(path.join(__dirname, '../vanillajs.png'));
+});
+
+app.listen(8076, '127.0.0.1', () => {
+    console.log('OIDC Redirect listening at http://127.0.0.1:8076');
+});
+  
